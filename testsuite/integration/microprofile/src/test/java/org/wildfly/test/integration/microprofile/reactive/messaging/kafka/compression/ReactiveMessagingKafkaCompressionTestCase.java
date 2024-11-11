77da23563c1279b2375ad298fd57f06a00d31a64
Kabir Khan <kkhan@redhat.com>
2024-11-11 15:02:19 +0100

    [WFLY-19932] Use Testcontainers to start Kafka rather than running embedded

--- testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/reactive/messaging/kafka/compression/ReactiveMessagingKafkaCompressionTestCase.java
+++ testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/reactive/messaging/kafka/compression/ReactiveMessagingKafkaCompressionTestCase.java
@@ -8,6 +8,7 @@ package org.wildfly.test.integration.microprofile.reactive.messaging.kafka.compr
 import jakarta.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.testcontainers.api.DockerRequired;
 import org.jboss.as.arquillian.api.ServerSetup;
 import org.jboss.as.test.shared.CLIServerSetupTask;
 import org.jboss.as.test.shared.PermissionUtils;
@@ -34,6 +35,7 @@ import java.util.concurrent.TimeUnit;
  */
 @RunWith(Arquillian.class)
 @ServerSetup({RunKafkaSetupTask.class, EnableReactiveExtensionsSetupTask.class})
+@DockerRequired
 public class ReactiveMessagingKafkaCompressionTestCase {
 
     private static final long TIMEOUT = TimeoutUtil.adjust(15000);
