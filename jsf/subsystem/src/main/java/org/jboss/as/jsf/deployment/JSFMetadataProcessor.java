8199bc10d17fe20872a12a4cc0d35045446128fd
mkuligPSI <mkulig@psi.pl>
2026-08-14 21:38:18 +0200

    PJFCB-22608 Set org.glassfish:jakarta.faces to 4.0.22 (#424)

--- jsf/subsystem/src/main/java/org/jboss/as/jsf/deployment/JSFMetadataProcessor.java
+++ jsf/subsystem/src/main/java/org/jboss/as/jsf/deployment/JSFMetadataProcessor.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 import jakarta.faces.application.ViewHandler;
 import org.jboss.as.controller.ModuleIdentifierUtil;
-import org.jboss.as.jsf.logging.JSFLogger;
 import org.jboss.as.server.deployment.DeploymentPhaseContext;
 import org.jboss.as.server.deployment.DeploymentUnit;
 import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
@@ -85,12 +84,6 @@ public class JSFMetadataProcessor implements DeploymentUnitProcessor {
             setContextParameterIfAbsent(webMetaData, WebConfiguration.BooleanWebContextInitParameter.DisallowDoctypeDecl.getQualifiedName(), disallowDoctypeDecl.toString());
         }
         if (webMetaData.getDistributable() != null) {
-            // Auto-disable lazy bean validation for distributable web application.
-            // This can otherwise cause missing @PreDestroy events.
-            String disabled = Boolean.toString(false);
-            if (!setContextParameterIfAbsent(webMetaData, WebConfiguration.BooleanWebContextInitParameter.EnableLazyBeanValidation.getQualifiedName(), disabled).equals(disabled)) {
-                JSFLogger.ROOT_LOGGER.lazyBeanValidationEnabled();
-            }
 
             String version = JsfVersionMarker.getVersion(deploymentUnit);
             // Disable counter-productive "distributable" logic in Mojarra implementation
